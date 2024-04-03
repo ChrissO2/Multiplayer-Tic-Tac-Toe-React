c9a6b27a3b4efbb451ce8fb900ca78fbdfb98712
Krzysztof Otreba <krzysztof.otreba02@gmail.com>
2024-04-03 14:11:31 +0200

    fixed

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -9,7 +9,7 @@ function Login({ setIsAuth }) {
 
   const cookies = new Cookies();
   const login = () => {
-    Axios.post(window.location.hostname + ':3001/login', {
+    Axios.post(window.location.hostname.slice(0, -1) + '1/login', {
       username,
       password,
     }).then((res) => {
